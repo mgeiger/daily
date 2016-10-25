9aa97d7e4798df0c9ddbba44f14c709f98b8a85e
Geiger <matthew.geiger@fmi.lcl>
2016-10-25 11:56:19 -0400

    Easy 001 completed. No bonus work.

---
+++ easy/001/easy_001.py
@@ -0,0 +1,4 @@
+person = input("Enter your name: ")
+age = input("Enter your age: ")
+username = input("Enter your username: ")
+print("your name is {}, you are {} years old, and your username is {}".format(person, age, username))
